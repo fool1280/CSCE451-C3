50a406f9de417e7500cd80f86f95fe8164b5777f
Anh (Henry) Nguyen <ng.huyanh.gd@gmail.com>
2023-11-28 13:36:09 -0600

    Update main.py

--- main.py
+++ main.py
@@ -3,11 +3,78 @@ from joblib import load
 import numpy as np
 from parsing import disassemble_and_process
 from stats import most_similar_top_5
+import requests
+import time
+import threading
 
-if __name__ == "__main__":
+def getInfo(file):
+    url = 'https://www.virustotal.com/api/v3/files/upload_url'
+    headers = {
+        'accept': 'application/json',
+        'x-apikey': 'bd162e9eb911d1ad4c964ccc45af431dfdaa24e96f6aba72245d808bb69e1298'
+    }
+    response = requests.get(url, headers=headers)
+    upload_url = response.json()['data']
+
+    files = { 'file': (file, open(file, 'rb'), 'application/octet-stream') }
+    headers = {
+        'accept': 'application/json',
+        'x-apikey': 'bd162e9eb911d1ad4c964ccc45af431dfdaa24e96f6aba72245d808bb69e1298'
+    }
+    response = requests.post(upload_url, files=files, headers=headers)
+    analysis_url = response.json()['data']['links']['self']
+
+    headers = {
+        'accept': 'application/json',
+        'x-apikey': 'bd162e9eb911d1ad4c964ccc45af431dfdaa24e96f6aba72245d808bb69e1298'
+    }
+
+    response = requests.get(analysis_url, headers=headers)
+    status = response.json()['data']['attributes']['status']
+    while status != 'completed':
+        time.sleep(90)
+        response = requests.get(analysis_url, headers=headers)
+        status = response.json()['data']['attributes']['status']
+    results = response.json()['data']['attributes']['results']
+
+    harmless = set()
+    malicious = set()
+    suspicious = set() 
+    undetected = set()
+    for engine in results:
+        if results[engine]['category'] == 'harmless':
+            harmless.add(engine)
+        elif results[engine]['category'] == 'malicious':
+            malicious.add(engine)
+        elif results[engine]['category'] == 'suspicious':
+            suspicious.add(engine)
+        elif results[engine]['category'] == 'undetected':
+            undetected.add(engine)
+
+    with open('av_report.txt', 'w') as file:
+        file.write("Harmless - AV thinks the file is not malicious ({}):\n".format(len(harmless)))
+        for engine in harmless:
+            file.write("    " + engine + '\n')
+
+        file.write("\nMalicious - AV thinks the file is malicious ({}):\n".format(len(malicious)))
+        for engine in malicious:
+            file.write("    " + engine + '\n')
+
+        file.write("\nSuspicious - AV thinks the file is suspicious ({}):\n".format(len(suspicious)))
+        for engine in suspicious:
+            file.write("    " + engine + '\n')
+
+        file.write("\nUndetected - AV has no opinion about this file ({}):\n".format(len(undetected)))
+        for engine in undetected:
+            file.write("    " + engine + '\n')
+
+    print("Anti-virus scan result output to av_report.txt")
+
+
+if __name__ == '__main__':
     # Check if an argument is provided
     if len(sys.argv) != 2:
-        print("Usage: python main.py <filename>")
+        print('Usage: python main.py <filename>')
         sys.exit(1)
 
     filename = sys.argv[1]
@@ -45,5 +112,14 @@ if __name__ == "__main__":
             new_row[opcode] += 1
         else:
             new_row['other'] += 1
+    info_thread = threading.Thread(target=getInfo, args=(filename,))
+    info_thread.start()
+    print("Waiting to get anti-vurus scan result...")
+
+    # Now call most_similar_top_5 in the main thread
     most_similar_top_5(new_row, prediction[0])
+
+    # After most_similar_top_5 has completed, wait for getInfo to complete
+    info_thread.join()
+    
 
